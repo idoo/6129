b15a0799f7ca0e0c2dda87579be8449c96d64ba2
Ivan Verevkin <ivan.p.verevkin@gmail.com>
2019-06-05 00:44:49 +0200

    Recreate the button for CSS-wizardry

---
+++ button/src/index.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<div>
+  <button 
+    class="button orange-button" 
+    role="button" 
+    aria-label="Delete" 
+    aria-roledescription="Delete button"
+    tabindex="1">
+    Delete
+  </button>
+</div>
+`;
